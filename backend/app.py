13881794345661862100ae354e792f647fbc8d7f
T1 <tkiet0012@gmail.com>
2025-11-09 01:44:16 +0100

    add cors config for backend to allow everything -> reduce access denied risk

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routes import router as routes_router
 
 app = FastAPI()
+# Allows all origins, methods, and headers, only for development
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(routes_router, prefix="/api")
 
 @app.get("/health")
